8ca52cf50a854e273cd0e78d5f1f824725d69aec
dhimanAnurag <agoodinternetuser@gmail.com>
2025-05-19 10:42:25 +0530

    first commit

---
+++ src/main/java/com/scaler/userservicedecmwfeve/UserservicedecmwfeveApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.userservicedecmwfeve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserservicedecmwfeveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserservicedecmwfeveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/userservicedecmwfeve/UserservicedecmwfeveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.userservicedecmwfeve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserservicedecmwfeveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
