11af932ce3ec17538e7d084dc7bdbb9ef454de45
dhimanAnurag <agoodinternetuser@gmail.com>
2025-07-17 19:38:25 +0530

    implemented the oauth2 via using spring security and in build classes and interfaces from spring boot

---
+++ src/main/java/com/scaler/userservicedecmwfeve/security/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.scaler.userservicedecmwfeve.security.repositories;
+import java.util.Optional;
+
+import com.scaler.userservicedecmwfeve.security.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
--- src/test/java/com/scaler/userservicedecmwfeve/UserservicedecmwfeveApplicationTests.java
+++ src/test/java/com/scaler/userservicedecmwfeve/UserservicedecmwfeveApplicationTests.java
@@ -1,13 +1,46 @@
 package com.scaler.userservicedecmwfeve;
 
+import com.scaler.userservicedecmwfeve.security.repositories.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.test.annotation.Commit;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserservicedecmwfeveApplicationTests {
+    @Autowired
+    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
 
     @Test
     void contextLoads() {
     }
+//        @Test
+//        @Commit
+//        void storedRegisteredClientIntoDB() {
+//            RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//                .clientId("oidc-client")
+//                .clientSecret("{noop}secret")
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .redirectUri("https://oauth.pstmn.io/v1/callback")
+//                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .scope("ADMIN")
+//                .scope("STUDENT")
+//                .scope("MENTOR") // Role
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//                 jpaRegisteredClientRepository.save(oidcClient);
+//        }
+    }
 
-}
+
